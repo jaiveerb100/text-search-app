50ab4cad3eed8cdc42b10701be1c34c322186e23
Jaiveer Bagga <jvcool1000@gmail.com>
2026-09-13 23:28:55 -0400

    Added pdf text extraction

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
+from pypdf import PdfReader
 
 app = FastAPI()
 
@@ -6,11 +7,24 @@ app = FastAPI()
 def read_root():
     return {"message": "Server is running!"}
 
+# endpoint to upload a PDF file
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     contents = await file.read()
-    
-    with open(f"uploads/{file.filename}", "wb") as f:
+
+    file_path = f"uploads/{file.filename}"
+    with open(file_path, "wb") as f:
         f.write(contents)
 
+    extracted_text = extract_text_from_pdf(file_path)
+    print(extracted_text)
+
     return {"filename": file.filename, "size": len(contents)}
+
+# function to extract text from PDF
+def extract_text_from_pdf(file_path: str) -> str:
+    reader = PdfReader(file_path)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+    return text
